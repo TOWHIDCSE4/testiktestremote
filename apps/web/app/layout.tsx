03b3fe77cb7fe2a5f88fcc8a7a3f1f9426f73580
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-08-03 08:48:23 +0800

    chore: Initial commit

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,13 @@
+import "./globals.css"
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,11 @@
-import { Button, Header } from "ui";
+import { Button, Header } from "ui"
 
 export default function Page() {
   return (
     <>
       <Header text="Web" />
       <Button />
+      <h2 className="text-blue-600">Test</h2>
     </>
-  );
+  )
 }
