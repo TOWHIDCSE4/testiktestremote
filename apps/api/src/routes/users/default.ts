f7026902d34f4c60c0d7813865121a5056e2a5d6
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-08-08 17:00:40 +0800

    fix(api): User will not be allowed to re use email on registration

--- apps/api/src/routes/users/default.ts
+++ apps/api/src/routes/users/default.ts
@@ -65,7 +65,7 @@ export const addUser = async (req: Request, res: Response) => {
     try {
       const getExistingUser = await Users.find({
         $or: [{ email }],
-        deletedAt: { $exists: false },
+        deletedAt: { $exists: true },
       })
       if (getExistingUser.length === 0) {
         const validateUserInput = UsersZodSchema.safeParse(newUser)
@@ -76,7 +76,9 @@ export const addUser = async (req: Request, res: Response) => {
           res.json({ error: true, message: validateUserInput.error })
         }
       } else {
-        res.status(400).json(ACCOUNT_ALREADY_EXISTS)
+        res
+          .status(400)
+          .json({ error: true, errMessage: ACCOUNT_ALREADY_EXISTS })
       }
     } catch (err: any) {
       const message = err.message ? err.message : UNKNOWN_ERROR_OCCURRED
