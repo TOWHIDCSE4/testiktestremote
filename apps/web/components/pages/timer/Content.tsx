bfbbb2928a354cdf0f753d13078fe5807c2955bf
jpmadrigal07 <jp.madrigal07@gmail.com>
2023-08-10 20:52:52 +0800

    fix(web): Conflict resolved (class styling conflict)

--- apps/web/components/pages/timer/Content.tsx
+++ apps/web/components/pages/timer/Content.tsx
@@ -32,7 +32,7 @@ const Content = () => {
   ]
 
   return (
-    <div className={`my-20 lg:ml-[305px] 2xl:ml-64`}>
+    <div className={`my-20`}>
       <div className="content px-4 md:px-7 lg:px-16 2xl:px-44 2xl:max-w-7xl mx-auto mt-28 ">
         <div className="flex justify-between items-center py-2">
           <div>
