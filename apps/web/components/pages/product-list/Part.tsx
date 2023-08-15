db8fa7249a608e556653aafd46c7996ae88307ef
Kylle Varon Madrigal <jp.madrigal07@gmail.com>
2023-08-15 09:51:58 +0800

    refactor: changed number of pages implementation

--- apps/web/components/pages/product-list/Part.tsx
+++ apps/web/components/pages/product-list/Part.tsx
@@ -324,7 +324,7 @@ const Part = ({
                   </button>
                   {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
                   {numberOfPages
-                    ? new Array(numberOfPages).fill().map((_, index) => (
+                    ? [...Array(numberOfPages)].map((_, index) => (
                         <button
                           key={index + 1}
                           onClick={() => setPage(index + 1)}
