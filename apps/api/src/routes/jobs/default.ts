470c1e767875381b10130586a69ab600da6218c9
abdulhai <abdulhai.elegant@gmail.com>
2023-09-28 20:02:42 +0500

    fixed issues

--- apps/api/src/routes/jobs/default.ts
+++ apps/api/src/routes/jobs/default.ts
@@ -64,8 +64,11 @@ export const addJob = async (req: Request, res: Response) => {
     const part = await Parts.findOne({
       _id: req.body.partId,
     })
+    const { status, drawingNumber, ...rest } = req.body
     const newJob = new Jobs({
-      ...req.body,
+      ...rest,
+      status: req.body.isStock ? "Active" : req.body.status,
+      drawingNumber: req.body.isStock ? undefined : req.body.drawingNumber,
       factoryId: part?.factoryId,
     })
     try {
