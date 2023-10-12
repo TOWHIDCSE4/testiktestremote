41cd570d18a31fdba8e95350f97c48a22b3b9405
abdulhai <abdulhai.elegant@gmail.com>
2023-10-12 13:02:27 +0500

    fix

--- apps/api/src/routes/timerLogs/globalLogs.ts
+++ apps/api/src/routes/timerLogs/globalLogs.ts
@@ -322,8 +322,56 @@ export const calculateGlobalMetrics = async (req: Request, res: Response) => {
 
     // Calculate the total time in hours
     const totalTime = dayjs(endTime).diff(startTime, "hour")
+    let query = {}
 
-    const partIds = await TimerLogs.distinct("partId")
+    if (locationId) {
+      //@ts-expect-error
+      const locationids = locationId
+        .split(",")
+        .map((e) => new mongoose.Types.ObjectId(e))
+      //@ts-expect-error
+      query.locationId = { $in: locationids }
+    }
+    if (factoryId) {
+      //@ts-expect-error
+      const factoryids = factoryId
+        .split(",")
+        .map((e) => new mongoose.Types.ObjectId(e))
+      //@ts-expect-error
+      query.factoryId = { $in: factoryids }
+    }
+    if (machineId) {
+      //@ts-expect-error
+      const machineids = machineId
+        .split(",")
+        .map((e) => new mongoose.Types.ObjectId(e))
+      //@ts-expect-error
+      query.machineId = { $in: machineids }
+    }
+    if (machineClassId) {
+      //@ts-expect-error
+      const machineClassids = machineClassId
+        .split(",")
+        .map((e) => new mongoose.Types.ObjectId(e))
+      //@ts-expect-error
+      query.machineClassId = { $in: machineClassids }
+    }
+    if (partId) {
+      //@ts-expect-error
+      const partids = partId
+        .split(",")
+        .map((e) => new mongoose.Types.ObjectId(e))
+      //@ts-expect-error
+      query.partId = { $in: partids }
+    }
+    if (startDate && endDate) {
+      //@ts-expect-error
+      query.createdAt = {
+        $gte: new Date(startDate as string),
+        $lt: new Date(endDate as string),
+      }
+    }
+    const partIds = await TimerLogs.distinct("partId", query)
     const aggregation = [
       {
         $match: {
@@ -337,56 +385,9 @@ export const calculateGlobalMetrics = async (req: Request, res: Response) => {
         },
       },
     ]
-    if (locationId) {
-      //@ts-expect-error
-      const locationids = locationId
-        .split(",")
-        .map((e) => new mongoose.Types.ObjectId(e))
-      //@ts-expect-error
-      aggregation[0].$match.locationId = { $in: locationids }
-    }
-    if (factoryId) {
-      //@ts-expect-error
-      const factoryids = factoryId
-        .split(",")
-        .map((e) => new mongoose.Types.ObjectId(e))
-      //@ts-expect-error
-      aggregation[0].$match.factoryId = { $in: factoryids }
-    }
-    if (machineId) {
-      //@ts-expect-error
-      const machineids = machineId
-        .split(",")
-        .map((e) => new mongoose.Types.ObjectId(e))
-      //@ts-expect-error
-      aggregation[0].$match.machineId = { $in: machineids }
-    }
-    if (machineClassId) {
-      //@ts-expect-error
-      const machineClassids = machineClassId
-        .split(",")
-        .map((e) => new mongoose.Types.ObjectId(e))
-      //@ts-expect-error
-      aggregation[0].$match.machineClassId = { $in: machineClassids }
-    }
-    if (partId) {
-      //@ts-expect-error
-      const partids = partId
-        .split(",")
-        .map((e) => new mongoose.Types.ObjectId(e))
-      //@ts-expect-error
-      aggregation[0].$match.partId = { $in: partids }
-    }
-    if (startDate && endDate) {
-      //@ts-expect-error
-      aggregation.$match.createdAt = {
-        $gte: new Date(startDate),
-        $lt: new Date(endDate),
-      }
-    }
+    const totalUnits = await timerLogs.find(query).countDocuments()
     const [result] = await parts.aggregate(aggregation).exec()
     const { totalTons } = result
-    const totalUnits = await timerLogs.countDocuments()
     const globalUnitsPerHour = totalUnits / totalTime
     const globalTonsPerHour = totalTons / totalTime
     res.json({
@@ -402,7 +403,7 @@ export const calculateGlobalMetrics = async (req: Request, res: Response) => {
     console.log(err)
     //@ts-expect-error
     const message = err.message ? err.message : UNKNOWN_ERROR_OCCURRED
-    Sentry.captureException(err)
+    // Sentry.captureException(err)
     return {
       error: true,
       message,
