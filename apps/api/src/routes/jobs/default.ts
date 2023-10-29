148ee48bab0799af4ca6946bc7fe87e5f51c3495
Abdul Hai <abdulhai.elegant@gmail.com>
2023-10-29 05:24:17 +0500

    fix - [AH] Stock job status testing filter out condition

--- apps/api/src/routes/jobs/default.ts
+++ apps/api/src/routes/jobs/default.ts
@@ -114,6 +114,7 @@ export const addJob = async (req: Request, res: Response) => {
           $and: [
             { status: { $ne: "Deleted" } },
             { status: { $ne: "Archived" } },
+            { status: { $ne: "Testing" } }, //TODO: discuss with jamiel
           ],
           $or: [{ deletedAt: { $exists: false } }, { deletedAt: null }],
         })
