f17fc942dd3be8c13451e152add3bf8826d4661a
Abdul Hai <abdulhai.elegant@gmail.com>
2023-11-13 20:48:26 +0500

    MI Tooltip By default close

--- apps/web/components/pages/order-flow/production-tracker/LogsTable/index.tsx
+++ apps/web/components/pages/order-flow/production-tracker/LogsTable/index.tsx
@@ -1292,7 +1292,6 @@ const LogsTable = ({
                                     </span>
                                   }
                                   trigger="hover"
-                                  defaultOpen
                                 >
                                   {typeof item.partId === "object"
                                     ? item.partId.name
