d4f6b3e498f1c66231669ce87c5378ceb5c6da0e
Muhammad Hanif <hanifeij@yahoo.co.id>
2023-12-11 12:04:34 +0700

    chore: disable persist query

--- apps/web/components/QueryWrapper.tsx
+++ apps/web/components/QueryWrapper.tsx
@@ -1,37 +1,30 @@
 "use client"
-import { QueryClient } from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client"
-import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister"
+// import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client"
+// import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister"
 import React from "react"
 
 const BUILD_ID = (process.env as any).CONFIG_BUILD_ID
 console.log("Current BUILD_ID:", BUILD_ID)
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      cacheTime: 1000 * 60 * 60 * 24,
-    },
-  },
+  defaultOptions: {},
 })
 
-const persister = createSyncStoragePersister({
-  storage: typeof window === "undefined" ? null : window.localStorage,
-})
+// const persister = createSyncStoragePersister({
+//   storage: typeof window === "undefined" ? null : window.localStorage,
+// })
 
 type Props = {
   children: React.ReactNode
 }
 
 const QueryWrapper = ({ children }: Props) => (
-  <PersistQueryClientProvider
-    client={queryClient}
-    persistOptions={{ persister, buster: BUILD_ID }}
-  >
+  <QueryClientProvider client={queryClient}>
     {children}
     <ReactQueryDevtools initialIsOpen={false} />
-  </PersistQueryClientProvider>
+  </QueryClientProvider>
 )
 
 export default QueryWrapper
