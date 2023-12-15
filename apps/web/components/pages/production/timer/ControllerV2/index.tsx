e22f5a1d1882e560923e51f191bdc6ac30acf5f1
dragon1227 <dragon99steel@gmail.com>
2023-12-15 12:17:43 +0800

    Fixed unit count arrangement

--- apps/web/components/pages/production/timer/ControllerV2/index.tsx
+++ apps/web/components/pages/production/timer/ControllerV2/index.tsx
@@ -96,15 +96,17 @@ const ControllerV2 = ({
                 <OperatorSelectComponent />
                 <JobSelectComponent />
               </div>
-              <div className="flex flex-wrap items-start justify-between w-full pt-1 sm:hidden">
+              <div className="flex flex-wrap items-start justify-between w-full gap-4 pt-1 sm:hidden">
                 <div className="pt-16">
                   <PauseButtonComponent />
                 </div>
-                <div className="flex flex-col items-center flex-1">
-                  <ResultsUnitCountComponent />
-                  <div className="relative flex flex-col items-center w-full gap-1">
-                    <OperatorSelectComponent />
-                    <JobSelectComponent />
+                <div className="relative flex-1">
+                  <div className="flex flex-col items-center w-fit">
+                    <ResultsUnitCountComponent />
+                    <div className="relative flex flex-col items-center w-full gap-1">
+                      <OperatorSelectComponent />
+                      <JobSelectComponent />
+                    </div>
                   </div>
                 </div>
               </div>
