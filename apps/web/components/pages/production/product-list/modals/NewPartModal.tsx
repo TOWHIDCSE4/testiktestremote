55f1607b877e86dfeb78618c25e97e51727912db
Muhammad Hanif <msungkar@iekomedia.com>
2024-01-18 11:25:57 +0700

    fix: placheolder should be pounds (#901)

--- apps/web/components/pages/production/product-list/modals/NewPartModal.tsx
+++ apps/web/components/pages/production/product-list/modals/NewPartModal.tsx
@@ -272,7 +272,7 @@ const NewPartModal = ({
                             isAddPartLoading || isUploadMediaFilesLoading
                           }
                           className={`mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-blue-950 sm:text-sm sm:leading-6 disabled:opacity-70`}
-                          placeholder="Tons"
+                          placeholder="Pounds"
                           {...register("tons", {
                             required: true,
                             valueAsNumber: true,
