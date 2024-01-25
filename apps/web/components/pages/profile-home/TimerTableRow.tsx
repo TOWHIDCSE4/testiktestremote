4f18f41d78962d8a823c903a11af7fe9a10cf133
samreen-rashid <rashid4008834@cloud.neduet.edu.pk>
2024-01-25 22:50:10 +0500

    added css and timer based status

--- apps/web/components/pages/profile-home/TimerTableRow.tsx
+++ apps/web/components/pages/profile-home/TimerTableRow.tsx
@@ -1,5 +1,10 @@
-import React from "react"
+"use client"
+import React, { useEffect } from "react"
 import useTotalTonsUnit from "../../../hooks/timers/useTotalTonsUnit"
+import { useSocket } from "../../../store/useSocket"
+import useTimerStatus from "../../../hooks/timers/useGetTimerStatus"
+import combineClasses from "../../../helpers/combineClasses"
+import useGetTimerIsEnded from "../../../hooks/timers/useGetTimerIsEnded"
 
 interface Props {
   rowData: any
@@ -7,27 +12,57 @@ interface Props {
 }
 
 const TimerTableRow: React.FC<Props> = async ({ rowData, index }) => {
+  const socket = useSocket((state: any) => state.instance)
+
   const query = useTotalTonsUnit({
     locationId: rowData.locationId,
     timerId: rowData._id,
   })
+
+  const status = useTimerStatus({
+    locationId: rowData.locationId,
+    timerId: rowData._id,
+  })
+
+  const isTimerEnded = useGetTimerIsEnded({ timerId: rowData._id })
+
+  useEffect(() => {
+    const handleTimerEvent = (data: any) => {
+      console.log("__EVENT_DATA", data)
+      if (data?.message === "refetch") {
+        query.refetch()
+        status.refetch()
+      }
+    }
+
+    if (socket) {
+      socket.on("timer-event", handleTimerEvent)
+    }
+
+    return () => {
+      if (socket) {
+        socket.off("timer-event", handleTimerEvent)
+      }
+    }
+  }, [socket])
+
   return (
     <tr>
       <td className="px-2 truncate">{rowData?.machine?.name}</td>
       <td className="line-clamp-1">{rowData?.part?.name}</td>
-      <td className="px-2">{query?.data?.item?.dailyUnits}</td>
+      <td className="px-2">{query?.data?.item?.dailyUnits ?? 0}</td>
       <td className="px-2">{query?.data?.item?.tons?.toFixed(2)}</td>
       <td>
         <div
-          className={`${
-            index == 0
-              ? "bg-green-500"
-              : index == 1
-              ? "bg-red-500"
-              : index == 2
-              ? "bg-yellow-500"
-              : "bg-transparent"
-          } border border-slate-500 rounded-full w-[6px] h-[6px]`}
+          className={combineClasses(
+            "border border-slate-500 rounded-full w-[6px] h-[6px]",
+            status.data?.items[0]?.status === "Loss" && "bg-red-500",
+            status.data?.items[0]?.status === "Gain" && "bg-green-500",
+            isTimerEnded.data?.items[0]?.endAt && "bg-gray-500",
+            isTimerEnded.data?.items[0]?.endAt &&
+              !status.data?.items[0]?.stopReason.includes("Unit Created") &&
+              "bg-yellow-500"
+          )}
         ></div>
       </td>
     </tr>
